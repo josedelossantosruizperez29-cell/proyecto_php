c125511af1debab1de22b0c79b0afaa121b7091a
jose <joseruizperez635@gamil.com>
2026-05-22 21:27:47 -0500

    Modelos y migraciones creados implemente Soft Delete

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Pest\Configuration\Project;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
@@ -23,12 +24,18 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+        public function project(){
+        return $this->hasMany(Project::class);
     }
 
     /**
